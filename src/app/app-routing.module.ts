1f3894ea67829bb7f4b8883a196132eeb3a6c0e1
windows <windows@microsoft.com>
2023-06-07 17:07:06 +0530

    chagne

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LadingpageComponent } from './ladingpage/ladingpage.component';
 import { NotfoundComponent } from './notfound/notfound.component';
--- src/app/ladingpage/ladingpage.component.ts
+++ src/app/ladingpage/ladingpage.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class LadingpageComponent {
 
+   a:number=80;
+   b:string="kumar";
+   t:number=80;
+   y:boolean =false;
+    name:any = "kumar";
+    myarr:number[]=[10,20,30,50,40,20];
+    mytype:string="text";
+    x:string = "5";
+    
+  myfun =()=>{
+    alert("hi");
+  }
+
+  myfun1 = function()
+  {
+    console.log("hi");
+  }
+
+  myret = ()=>{
+    return 400;
+  }
+
+
+
+
+   constructor()
+  {
+    let d:number =8000;
+    let mynew = ()=>{
+      alert("hi");
+    }
+  }
+
+
+
+
+  
 }
