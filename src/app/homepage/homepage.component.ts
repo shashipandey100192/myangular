ea2ec19e120cb2103ef8b514d6799e326bbb8542
windows <windows@microsoft.com>
2023-06-08 17:08:02 +0530

    change

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+
+b:number=0;
+c:any;
+d:number=0;
+f:number=0;
+total:number=0;
+mydate = Date.now();
+myfun = ()=>{
+alert("hi");
+}
+
+ngOnInit() {
+  this.myfun();  
+  this.myfirst();  
+  this.mynumber();
+}
+
+    myfirst():void
+    {
+    this.b=500;
+      // alert(this.b);
+      this.c= prompt("enter name");
+      // document.bgColor=this.c;
+      document.body.style.backgroundColor=`${this.c}`;
+    }
+   mynumber():void
+   {
+      this.d=800;
+      this.f=400;
+    this.total = this.d+this.f;
+    console.log(this.total);
+
+   }
+    
+
 
 }
