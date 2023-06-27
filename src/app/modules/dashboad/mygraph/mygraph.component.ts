4f1d080f41d55cd28ca3f050264ce113b1e9102c
windows <windows@microsoft.com>
2023-06-27 17:03:42 +0530

    forloop

--- src/app/modules/dashboad/mygraph/mygraph.component.ts
+++ src/app/modules/dashboad/mygraph/mygraph.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class MygraphComponent {
 
+
+
+   mylist=["Home","About","Contact","service","other","form","select"];
+
+   myobject =[
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'kumar',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'pankaj',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'mohan',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'sonhan',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'},
+    {name:'ravi',age:20,phone:'5457874878',address:'new delhi'}
+   ];
 }
