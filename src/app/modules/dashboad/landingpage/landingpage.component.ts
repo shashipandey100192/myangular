8732f818ad948997306f7c9b0aa21277c71911c5
windows <windows@microsoft.com>
2023-06-29 17:07:57 +0530

    get methods

--- src/app/modules/dashboad/landingpage/landingpage.component.ts
+++ src/app/modules/dashboad/landingpage/landingpage.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';;
+import { GenralService } from '../../services/genral.service';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrls: ['./landingpage.component.scss']
 })
-export class LandingpageComponent {
+export class LandingpageComponent implements OnInit{
+constructor(private myservice:GenralService){}
+
+myarry:any
+myalldata():void{
+  this.myservice.mydata().subscribe((e)=>{
+    
+    this.myarry=e;
+    console.log(this.myarry);
+  })
+}
+ngOnInit(): void {
+  this.myalldata();  
+
+}
+
+
+
+
 
 }
--- src/app/modules/services/genral.service.ts
+++ src/app/modules/services/genral.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import  {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenralService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+  public myurl ="https://jsonplaceholder.typicode.com/posts";
+
+  
+mydata()
+{
+ return this.http.get(this.myurl);
+}
+
+
 }
